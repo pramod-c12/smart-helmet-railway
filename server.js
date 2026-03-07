279ca794cfa2502dd4c79f05e8441e883269d616
pramod-c12 <pchidambara0625@gmail.com>
2026-03-08 00:18:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const WebSocket = require("ws");
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 const server = app.listen(PORT, () => {
   console.log("Server running on port", PORT);
@@ -32,4 +32,5 @@ wss.on("connection", (ws) => {
   });
 
 });
+
 
