6f6edee16da5db8504c7558118d6fbdf30906414
pramod-c12 <pchidambara0625@gmail.com>
2026-03-07 23:54:41 +0530

    Update port number in server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const WebSocket = require("ws");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const server = app.listen(PORT, () => {
   console.log("Server running on port", PORT);
@@ -32,3 +32,4 @@ wss.on("connection", (ws) => {
   });
 
 });
+
